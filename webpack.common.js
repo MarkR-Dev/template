e87c3fe2989bf1fb6efe8b73e2fa6cdbf12a7d28
Mark Rodgers <markrodgers.dev@gmail.com>
2024-08-12 19:16:32 +0100

    Add html-loader for images

--- webpack.common.js
+++ webpack.common.js
@@ -24,6 +24,10 @@ module.exports = {
         type: 'asset/resource',
       },
       {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
         test: /\.(?:js|mjs|cjs)$/,
         exclude: /node_modules/,
         use: {
